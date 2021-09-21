6030b55d74b6fe0f66d8bcf68d3a5622b72d2225
Kevin Peng <peng.kevin@me.com>
2021-09-20 23:25:31 -0400

    Add new API for extracting Pasteboard

--- Sources/Pasteboard/Pasteboard.swift
+++ Sources/Pasteboard/Pasteboard.swift
@@ -4,15 +4,21 @@ public func processPasteboard(
     modifier: (String) throws -> String
 ) rethrows {
     let pasteboard = NSPasteboard.general
+    let typeToCheck = NSPasteboard.PasteboardType.string
+    let string = getPasteboardContent()
+    let newString = try modifier(string)
+    pasteboard.clearContents()
+    pasteboard.setString(newString, forType: typeToCheck)
+}
+
+public func getPasteboardContent() -> String {
+    let pasteboard = NSPasteboard.general
     if let items = pasteboard.pasteboardItems {
         let typeToCheck = NSPasteboard.PasteboardType.string
 
         for element in items {
             if let string = element.string(forType: typeToCheck) {
-                let newString = try modifier(string)
-                pasteboard.clearContents()
-                pasteboard.setString(newString, forType: typeToCheck)
-                return
+                return string
             } else {
                 print("Pasteboard doesn't contain string")
             }
@@ -20,4 +26,5 @@ public func processPasteboard(
     } else {
         print("Pasteboard is empty!")
     }
+    return ""
 }
